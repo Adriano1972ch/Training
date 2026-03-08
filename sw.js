250256452d41d664e20759ed8622b9be56f3dda3
donateotraingsplan <adriano@donateo.com>
2026-03-08 20:18:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "training-app-v10";
-const APP_SHELL = [
+const CACHE_NAME = "training-app-v4";
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
@@ -10,37 +10,24 @@ const APP_SHELL = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    await self.clients.claim();
-  })());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith((async () => {
-    try {
-      const networkResponse = await fetch(event.request);
-      const url = new URL(event.request.url);
-
-      if (url.origin === self.location.origin) {
-        const cache = await caches.open(CACHE_NAME);
-        cache.put(event.request, networkResponse.clone());
-      }
-
-      return networkResponse;
-    } catch (error) {
-      const cachedResponse = await caches.match(event.request);
-      if (cachedResponse) return cachedResponse;
-      throw error;
-    }
-  })());
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
